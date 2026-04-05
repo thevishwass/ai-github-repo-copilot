f277ef4af822ba809969f3d7d1b0d9f4a58b636b
Vishwas Singh <thevishwass@gmail.com>
2026-04-05 12:49:49 +0530

    Refactor server.js to clean up configurations
    Removed redundant dotenv configuration and CORS setup.

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,37 @@
 const dotenv = require("dotenv")
+dotenv.config()
+
+const express = require("express")
+const cors = require("cors")
 
 const connectDB = require("./config/db.js")
 
-require("dotenv").config()
-
-const express = require("express")
-
 const repoRoutes = require("./routes/repoRoutes")
 const chatRoutes = require("./routes/chatRoutes")
-
 const authRoutes = require("./routes/authRoutes")
+
 const app = express()
+
+// CORS
+app.use(cors({
+  origin: "*"
+}))
 
 app.use(express.json())
 
+// routes
 app.use("/api/repo", repoRoutes)
 app.use("/api/chat", chatRoutes)
-
 app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
-  res.send("Server is running on NODEMON")
+  res.send("Server is running")
 })
-
-console.log(typeof cloneRepo, "Hi")
-console.log(typeof scanFiles)
-
-
-const cors = require("cors")
-
-app.use(cors())
-app.use(
-  cors({
-    origin: [
-      "*"
-    ],
-    credentials: true
-  })
-)
-
-
-
-dotenv.config()
 
 connectDB()
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
- console.log(`Server is running O_O`)
+  console.log("Server is running O_O")
 })
